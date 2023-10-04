e081d56acb507e65f3dc93c1b2c292df25777847
oluleye oluseun <oluseun.oluleye@gmail.com>
2023-10-04 15:13:59 +0100

    Footer Added

--- resources/js/Layouts/Frontend.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="bg-slate-200 dark:bg-slate-900">
-        <main class="min-h-screen">
-            <slot /> 
-        </main>
-    </div>
-</template>
-
-<script setup></script>
---
+++ resources/js/Layouts/FrontendLayout.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-slate-200 dark:bg-slate-900">
+        <Header />
+        <main class="min-h-screen">
+            <slot />
+        </main>
+
+        <Footer />
+    </div>
+</template>
+
+<script setup>
+import Footer from "@/Components/Frontend/Footer.vue";
+import Header from "@/Components/Frontend/Header.vue";
+</script>
